a79164e00b6c5e2684c0008a667b186e632d4732
Juan Velazquez <velazquezrj2012@licifug.ugto.mx>
2021-03-03 17:41:39 -0600

    Sites comparison: get data to plot

--- plot/comparison_across_sites.R
+++ plot/comparison_across_sites.R
@@ -1,4 +1,41 @@
 comparison_across_sites <- function(site){
+  meanSite = data.frame()
+  for (j in seq(1,length(site))){
+    for (k in seq(1,14)){
+      siteData = as.numeric(unlist(listSpheres[[site[j]]][k]))
+      
+      meanSite[k,j] = mean(siteData)
+      
+      ##DIFERENCE BETWEEN MAGNITUDE AND COMPLEX
+      #diff_Mag_Comp[k,j] = mean(magData) - mean(compData)
+      #diff_Perc_Mag_Comp[k,j] = 100*abs(mean(magData) - mean(compData))/mean(magData)
+      
+      ##STATISTICAL TESTS (COMPARE MEANS)
+      
+    }
+    
+    id = data[site[j],"id"]
+    sid <- as.matrix(rep(id,14))
+    sph <- as.matrix(1:14)
+    
+    data_Site <- data.frame(sid, sph, meanSite[,j])
+    
+    if (j==1){
+      dataTmp = rbind(data.frame(), data_Site)
+      #corrTmp = rbind(data.frame(), corr_Mag_Comp)
+      #PearsonTmp = rbind(data.frame(), Pearson_test)
+    }
+    else{
+      dataSite2plot = rbind(dataTmp, data_Site)
+      #dataCorrelation = rbind(corrTmp, corr_Mag_Comp)
+      #dataPearson = rbind(PearsonTmp, Pearson_test)
+      dataTmp <- dataSite2plot
+      #corrTmp <- dataCorrelation
+      #PearsonTmp <- dataPearson
+    }
+  }
+  
+  ##ONE-WAY ANOVA##
   multComparisons <- list()
   for (j in seq(1,14)){
     flag = 1
@@ -22,5 +59,10 @@ comparison_across_sites <- function(site){
     multComparisons[j] = TukeyHSD(res.aov)
   }
   
-  return(multComparisons)
+  colnames(dataSite2plot) <- c('Site', 'Sphere', 'Mean')
+  
+  returnComparison <- list("dataSite" = dataSite2plot,
+                           "ANOVA" = multComparisons)
+  
+  return(returnComparison)
 }
