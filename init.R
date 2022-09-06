e142f4478b2a35104611b6f64ac31757cd693d52
Juan <velazquezrj2012@licifug.ugto.mx>
2022-09-05 22:19:44 -0400

    Load devtools

--- init.R
+++ init.R
@@ -16,4 +16,5 @@ install_if_missing = function(p) {
 invisible(sapply(my_packages, install_if_missing))
 
 # Install GRousselet/rogme package
+library(devtools)
 devtools::install_github("GRousselet/rogme")
